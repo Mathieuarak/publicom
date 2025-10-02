d0ebfed93bc331a7d9612f0dcab0a3cd269aa31a
GalaxyJay <borcedre@gmail.com>
2025-10-02 08:39:04 +0200

    modifcation page visu_message

--- app/Views/visu_message.php
+++ app/Views/visu_message.php
@@ -5,8 +5,20 @@
     <title>Publicom</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link rel="stylesheet" href="css/visu_message.css">
 </head>
 <body>
     <h1>visualisation des message de {nom de la comunne}</h1>
+
+    <a class="bouton" href='#'> Précédent </a>
+    <article class="visu_message">
+        <h2> Titre du message </h2>
+        <p>Hanc regionem praestitutis celebritati diebus invadere parans dux ante edictus per solitudines<BR>
+            Aboraeque amnis herbidas ripas, suorum indicio proditus, qui admissi flagitii metu exagitati ad <BR>
+            praesidia descivere Romana. absque ullo egressus effectu deinde tabescebat immobilis.<p>
+        
+    </article>
+    <a class="bouton" href='#'> Suivant </a>
+
 </body>
 </html>
