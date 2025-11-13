c2738ab4944c0dac52931a7fced3c78139249844
Mathieu Arakelian <mathieuarakeian193@gmail.com>
2025-11-13 09:05:42 +0100

    modif sur PanneauModif

--- app/Views/PanneauModif.php
+++ app/Views/PanneauModif.php
@@ -3,39 +3,31 @@
 
 <h1>Modifier un panneau de la commune de (commune)</h1>
 
-<?php if (empty($panneau)): ?>
 
-    <p>Le panneau demandé n’existe pas.</p>
-    <p><a href="<?= url_to('panneauListe') ?>">Retour à la liste</a></p>
+<form action="<?= url_to('panneauUpdate') ?>" method="post">
 
-<?php else: ?>
-    
-    <form action="<?= url_to('panneauUpdate') ?>" method="post">
+    <input type="hidden" name="id" value="<?= esc($panneau['ID']) ?>">
+    <p>
+        <label for="numero">Numéro :</label>
+        <input type="text" name="numero" id="numero"
+            value="<?= esc(old('numero', $panneau['NUMERO'])) ?>">
+    </p>
 
-        <input type="hidden" name="id" value="<?= esc($panneau['ID']) ?>">
-        <p>
-            <label for="numero">Numéro :</label>
-            <input type="text" name="numero" id="numero"
-                   value="<?= esc(old('numero', $panneau['NUMERO'])) ?>">
-        </p>
+    <p>
+        <label for="latitude">Latitude :</label>
+        <input type="text" name="latitude" id="latitude"
+            value="<?= esc(old('latitude', $panneau['LATITUDE'])) ?>">
+    </p>
 
-        <p>
-            <label for="latitude">Latitude :</label>
-            <input type="text" name="latitude" id="latitude"
-                   value="<?= esc(old('latitude', $panneau['LATITUDE'])) ?>">
-        </p>
+    <p>
+        <label for="longitude">Longitude :</label>
+        <input type="text" name="longitude" id="longitude"
+            value="<?= esc(old('longitude', $panneau['LONGITUDE'])) ?>">
+    </p>
 
-        <p>
-            <label for="longitude">Longitude :</label>
-            <input type="text" name="longitude" id="longitude"
-                   value="<?= esc(old('longitude', $panneau['LONGITUDE'])) ?>">
-        </p>
+    <button type="submit">Valider</button>
+    <a href="<?= url_to('panneauListe') ?>">Annuler</a>
+</form>
 
-        <button type="submit">Valider</button>
-        <a href="<?= url_to('panneauListe') ?>">Annuler</a>
-    </form>
-
-<?php endif; ?>
 
 <?= $this->endSection() ?>
-
