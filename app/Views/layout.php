0b9c98949ff20263365b7ac60a4524d0d4366360
Julien BONNAFE <j.bonnafe@lyceeclaudenougaro82300.onmicrosoft.com>
2025-11-13 09:05:40 +0100

    version anterieur layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -6,29 +6,10 @@
     <title>Publicom</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/menu.css">
     <link rel="stylesheet" href="css/tableaux.css">
     <link rel="stylesheet" href="css/form.css">
     <link rel="stylesheet" href="css/visu_message.css">
-    <style>
-        .card {
-            box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
-        }
-        .table-hover tbody tr:hover {
-            background-color: #f8f9fc;
-        }
-        .btn-icon {
-            padding: 0.375rem 0.75rem;
-        }
-        .category-header {
-            background: linear-gradient(45deg, #4e73df, #224abe);
-            color: white;
-            padding: 2rem 0;
-            margin-bottom: 2rem;
-        }
-    </style>
 
 </head>
 
@@ -52,9 +33,7 @@
             </ul>
     </nav>
 <?php } ?>
-<?= $this->renderSection('content') ?>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<?= $this->renderSection('contenu') ?>
 </body>
 
 </html>
